559648e2db9631c357c38d585fd1bc6a5b68cc8f
nonhlanhla-tshimologong <99407242+nonhlanhla-tshimologong@users.noreply.github.com>
2022-03-03 11:05:31 +0200

    Add files via upload

---
+++ work1.js
@@ -0,0 +1,20 @@
+function dataTypeSample(){
+    let productName = 'product name';
+    let productCount = 5;
+    let isActive = true;
+    let value = null;
+    let result;
+    let sellByDate = new Date();
+
+    let product = {
+        description: 'this is the product object'
+    };
+    let products = [];
+
+    console.log(typeof productName);
+    console.log(typeof productCount);
+    console.log(typeof isActive);
+    console.log(typeof value);
+    console.log(typeof result);
+    console.log(typeof sellByDate);
+}
